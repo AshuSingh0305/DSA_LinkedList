df3d8850bc5c6c6fa46bceeab963edea0142b431
Ashu Singh <ashu.singh2001.as@gmail.com>
2022-07-11 21:18:18 +0530

    update

--- LinkedList/LinkedListUsingJava.java
+++ LinkedList/LinkedListUsingJava.java
@@ -1,7 +1,6 @@
 package DataStructure.LinkedList;
 
-import java.util.Arrays;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class LinkedListUsingJava {
     private Node first;
@@ -35,17 +34,19 @@ public class LinkedListUsingJava {
         }
         size++;
     }
-    public void deleteFirst(){
+    public int deleteFirst(){
         if(first==null)
             throw new NoSuchElementException();
+        int temp = first.value;
         if(first==last){
             first=last=null;
-            return;
+            return temp;
         }
         Node second = first.next;
         first.next=null;
         first=second;
         size--;
+        return temp;
     }
     public void deleteLast(){
 
@@ -94,6 +95,13 @@ public class LinkedListUsingJava {
         return -1;
     }
 
+    public void reversesecond(LinkedListUsingJava list, LinkedListUsingJava list2){
+        int size=list.size;
+        for(int i=0;i<size;i++){
+            int temp = list.deleteFirst();
+            list2.addFirst(temp);
+        }
+    }
     public void reverse(){
         Node reverseFirst=null;
         Node reverseLast=null;
@@ -151,25 +159,276 @@ public class LinkedListUsingJava {
         System.out.println(pointer1.value);
     }
 
+    public void reverseB(){
+        reverseB(first);
+    }
+
+    private Node reverseB(Node first){
+        Node pre = null;
+        Node curr = first;
+        Node next = null;
+        while(curr!=null){
+            next = curr.next;
+            curr.next=pre;
+            pre=curr;
+            curr=next;
+        }
+        first=pre;
+        return first;
+    }
+
+    public void reverseInGroupOfGivenSize(){
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        reverseInGroupOfGivenSize(first, k);
+    }
+
+    private Node reverseInGroupOfGivenSize(Node first, int k){
+        if(first==null){
+            return null;
+        }
+
+        Node curr = first;
+        Node pre = null;
+        Node next = null;
+        int count=0;
+        while(curr != null && count < k){
+            next = curr.next;
+            curr.next=pre;
+            pre=curr;
+            curr=next;
+            count++;
+        }
+
+        if(next != null){
+            first.next = reverseInGroupOfGivenSize(next,k);
+        }
+        return pre;
+    }
+
+    public void detectLoop(){
+        boolean ans = detectLoop(first);
+    }
+
+    private boolean detectLoop(Node first){
+        Node temp = first;
+        while(temp.next!=null){
+            if(temp.value==0) return true;
+            temp.value=0;
+            temp=temp.next;
+        }
+        return false;
+    }
+
+    public void removeLoop(){
+        removeLoop(first);
+    }
+    private void removeLoop(Node first){
+        HashSet<Node> set = new HashSet<>();
+        Node pre = null;
+        Node curr = pre;
+        while (curr!=null){
+            if(set.contains(curr)){
+                pre.next=null;
+                return;
+            }else{
+                set.add(curr);
+                pre=curr;
+                curr=curr.next;
+            }
+        }
+
+    }
+    public void removeDuplicates(){
+        removeDuplicates(first);
+
+    }
+    private void removeDuplicates(Node first){
+        Node pre = first;
+        Node curr = first.next;
+        Node next = first.next;
+        while(curr!=null){
+            if(curr.value== pre.value){
+                curr=curr.next;
+                next=null;
+                next=curr;
+                if(curr==null){
+                    pre.next=null;
+                }
+            }else{
+                pre.next=curr;
+                pre=curr;
+                curr=curr.next;
+                next=curr;
+            }
+        }
+    }
+
+    public void removeDuplicateUnSorted(){
+        removeDuplicateUnSorted(first);
+    }
+    private void removeDuplicateUnSorted(Node head){
+        HashSet<Integer> set = new HashSet<>();
+        Node curr = head;
+        Node temp=curr;
+        Node next = curr.next;
+        set.add(curr.value);
+        while (next!=null){
+            if(set.contains(next.value)){
+                next=next.next;
+                temp.next=null;
+                curr.next=next;
+                temp=curr;
+                if(curr.next==null) break;
+            }else{
+                set.add(next.value);
+                curr.next=next;
+                curr=curr.next;
+                temp=temp.next;
+                next=curr.next;
+            }
+        }
+    }
+
+    public void moveLastToFirst(){
+        Node head = moveLastToFirst(first);
+    }
+    private Node moveLastToFirst(Node head){
+        Node curr = head;
+        Node temp = head.next;
+        while(temp.next!=null){
+            temp=temp.next;
+            curr=curr.next;
+        }
+        temp.next=head;
+        curr.next=null;
+        head=temp;
+        return head;
+    }
+
+    public void add1ToNumber(){
+        Node head = reverseB(first);
+        Node temp=head;
+        int carry=1;
+        while(temp!=null){
+            int num = temp.value;
+            if(num==9 && carry==1){
+                if(temp.next==null){
+                    temp.value=10;
+                    temp=temp.next;
+                    break;
+                }
+                temp.value=0;
+                carry=1;
+                temp=temp.next;
+            }else{
+                num+=carry;
+                temp.value=num;
+                carry=0;
+                break;
+            }
+        }
+        reverseB(head);
+    }
+
+    public void palindrome(){
+        boolean ans = palindrome(first);
+        System.out.println(ans);
+    }
+
+    private boolean palindrome(Node head){
+        Node temp = head;
+        int count=1;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp=head;
+        Node pre=null;
+        for(int i=0;i<count/2;i++){
+            // pre=temp;
+            temp=temp.next;
+        }
+        if(count%2==0){
+            pre = reverseB(temp);
+        }else{
+            pre = reverseB(temp.next);
+        }
+        temp=head;
+        while(pre!=null){
+            if(temp.value!=pre.value){
+                return false;
+            }
+            temp=temp.next;
+            pre=pre.next;
+        }
+        return true;
+    }
+
+    public Node deleteNode(Node head,int d)
+    {
+        //Add your code here.
+        Node temp = head;
+        Node pre = null;
+        if(head.value==d){
+            head=head.next;
+            return head;
+        }
+        while (true){
+            if(temp.value==d){
+                pre.next=temp.next;
+                break;
+            }
+            pre=temp;
+            temp=temp.next;
+        }
+        return head;
+
+    }
+    public void deleteNode(){
+        Scanner sc= new Scanner(System.in);
+        int d=sc.nextInt();
+        Node ans = deleteNode(first, d);
+        reverseB(ans);
+    }
+//    public void addTwoNumbers(){
+//        addTwoNumbers(first);
+//    }
+//
+//    public
+
+
+
     public static void main(String[] args) {
         var LinkedList = new LinkedListUsingJava();
-//        LinkedList.addLast(10);
-//        LinkedList.addLast(20);
-        LinkedList.addLast(30);
-        LinkedList.addLast(40);
-        LinkedList.addLast(50);
-        LinkedList.addLast(60);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=0;i<n;i++){
+            LinkedList.addLast(sc.nextInt());
+        }
+        LinkedList.deleteNode();
+//        LinkedList.palindrome();
+//        LinkedList.addTwoNumbers();
+//        LinkedList.add1ToNumber();
+//        LinkedList.moveLastToFirst();
+//        LinkedList.removeDuplicateUnSorted();
+//        LinkedList.removeDuplicates();
+//        LinkedList.removeLoop();
+//        LinkedList.detectLoop();
+//        LinkedList.reverseInGroupOfGivenSize();
+//        LinkedList.reverseB();
 //        LinkedList.deleteLast();
-        LinkedList.reverse();
-        int[] array = LinkedList.toArray();
-        System.out.println(Arrays.toString(array));
-        LinkedList.reverse();
-        array=LinkedList.toArray();
-        System.out.println(Arrays.toString(array));
+//        LinkedList.reverse();
+//        int[] array = LinkedList.toArray();
+//        System.out.println(Arrays.toString(array));
+//        LinkedList.reverse();
+//        array=LinkedList.toArray();
+//        System.out.println(Arrays.toString(array));
+//        LinkedList.findKthNode(2);
 //        LinkedList.deleteLast();
 //        LinkedList.deleteFirst();
 //        System.out.println(LinkedList.contains(10));
 //        System.out.println(LinkedList.indexOf(40));
+//        LinkedList.reversesecond(LinkedList, List2);
     }
 }
 
